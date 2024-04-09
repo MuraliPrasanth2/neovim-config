abc82a90e7b40b41c218a72349c9873473f97fef
Murali Prasanth <mprasanth18@gmail.com>
2024-04-09 12:41:29 +0530

    added config for rainbow in the tree-sitter config.

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -68,6 +68,15 @@ return {
                         include_surrounding_whitespace = true,
                     },
                 },
+                rainbow = {
+                    enable = true,
+                    -- list of languages you want to disable the plugin for
+                    -- disable = { 'jsx', 'cpp' }, 
+                    -- Which query to use for finding delimiters
+                    query = 'rainbow-parens',
+                    -- Highlight the entire buffer all at once
+                    strategy = require('ts-rainbow').strategy.global,
+                },
 
             })
         end,
