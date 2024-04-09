884e215a5ce6330e65bad4225ce81fe4cfc61cec
Murali Prasanth <mprasanth18@gmail.com>
2024-04-09 12:33:55 +0530

    commented all the keymap for selecting all as it will overshadow the default increment key combo in neovim <C_a>.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true })
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true })
