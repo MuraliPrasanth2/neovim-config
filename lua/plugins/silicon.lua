4fd485078853148616dbabcabb0ffbf3af0befc8
Murali Prasanth <mprasanth18@gmail.com>
2024-05-17 14:49:28 +0530

    modified Silicon to look good for screenshot

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -1,13 +1,23 @@
 return {
-	"michaelrommel/nvim-silicon",
-	lazy = true,
-	cmd = "Silicon",
-	config = function()
-		require("silicon").setup({
-			-- Configuration here, or leave empty to use defaults
-			font = "JetBrains Mono NL=34;Noto Color Emoji=34",
-            theme = "Dracula",
-            background = "#94e2d5"
-		})
-	end
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+        require("silicon").setup({
+            -- Configuration here, or leave empty to use defaults
+            font = "JetBrains Mono NL=34;Noto Color Emoji=34",
+            theme = "OneHalfDark",
+            background = "#ADB7C1",
+            -- window_title = function()
+            --     return vim.fn.fnamemodify(
+            --         vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+            --         ":t"
+            --     )
+            -- end,
+            window_title = function()
+                -- return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ':~:.')
+                return "something"
+            end,
+        })
+    end
 }
