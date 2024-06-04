5f8e43d625d832299dfd58a9958784840303da88
Murali Prasanth <mprasanth18@gmail.com>
2024-06-04 10:38:19 +0530

    added keymap for python launching

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,3 @@
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true })
+vim.keymap.set("n", "<leader>P", ":!python3 main.py<CR>", { noremap = true })
