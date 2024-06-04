3b15bd8cc6f5ac60c1c32888980a71da2ed885b7
Murali Prasanth <mprasanth18@gmail.com>
2024-06-04 10:36:00 +0530

    added formatter for python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,6 +11,7 @@ return {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.completion.spell,
                 null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
                 require("none-ls.diagnostics.eslint_d"), -- requires none-ls-extras.nvim
             },
         })
