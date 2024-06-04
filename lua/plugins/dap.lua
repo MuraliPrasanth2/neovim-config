874371e30652389b8f02bacba602e76af41fef83
Murali Prasanth <mprasanth18@gmail.com>
2024-06-04 10:36:12 +0530

    modified dap still not working for js

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,6 +1,85 @@
 return {
 	"mfussenegger/nvim-dap",
-    config = function()
-         
-    end
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		-- setting up debugger for js
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "https://127.0.0.1",
+			port = "8123",
+			executable = {
+				command = "js-degug-adapter",
+			},
+		}
+		for _, language in ipairs({ "typescript", "javascript", "javascriptreact", "typescriptreact" }) do
+			require("dap").configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					runtimeExecutable = "node",
+				},
+			}
+		end
+		-- making nvim-dap-ui to oepn up when for nvim-dap events
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		-- dap keymaps
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end)
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end)
+		vim.keymap.set("n", "<Leader>db", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>dlp", function()
+			dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end)
+		vim.keymap.set("n", "<Leader>dr", function()
+			dap.repl.open()
+		end)
+		vim.keymap.set("n", "<Leader>dl", function()
+			dap.run_last()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+			require("dap.ui.widgets").hover()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+			require("dap.ui.widgets").preview()
+		end)
+		vim.keymap.set("n", "<Leader>df", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.frames)
+		end)
+		vim.keymap.set("n", "<Leader>ds", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.scopes)
+		end)
+	end,
 }
